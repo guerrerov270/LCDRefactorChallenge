245ba2b8ad72fe57e7fad93992d82a6a7d90066e
guerrerov270 <jguerrero089@gmail.com>
2018-05-28 23:38:49 -0500

    Subida del proyecto para análisis previo con Sonar

---
+++ src/main/java/lcd_refactor/LCD_PSL/App.java
@@ -0,0 +1,13 @@
+package lcd_refactor.LCD_PSL;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/lcd_refactor/LCD_PSL/AppTest.java
@@ -0,0 +1,20 @@
+package lcd_refactor.LCD_PSL;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
